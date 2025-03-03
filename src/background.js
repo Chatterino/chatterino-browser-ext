d0864f1764e363372169ef9eedb16a30bef062f6
nerix <nerixdev@outlook.de>
2025-03-03 20:06:28 +0100

    refactor: migrate to manifest v3 (#100)
    * refactor: migrate to manifest v3
    
    * fix: formatting
    
    * ci: update format stuff
    
    * fix: remove explicit version
    
    * ci: add a build stage
    
    * chore: add log when exiting
    
    * fix: rename workflow

--- src/background.js
+++ src/background.js
@@ -17,13 +17,51 @@ const ignoredPages = new Set([
   'wallet',
 ]);
 
-const attachedWindows = {};
-// we try to detach every native window once at start in case someone reloaded
-// the extension
-let detachedWindowsCache = {};
+class AttachedWindows {
+  /** @param {number} winID */
+  static async isAttached(winID) {
+    const windows = await AttachedWindows.#load();
+    return winID in windows;
+  }
+
+  /** @param {number} winID */
+  static async markAttached(winID) {
+    const windows = await AttachedWindows.#load();
+    windows[winID] = true;
+    await AttachedWindows.#save(windows);
+  }
+
+  /** @param {number} winID */
+  static async markDetatched(winID) {
+    const windows = await AttachedWindows.#load();
+    delete windows[winID];
+    await AttachedWindows.#save(windows);
+  }
+
+  /** @returns {Promise<number[]>} */
+  static async detachAll() {
+    const windows = await AttachedWindows.#load();
+    await AttachedWindows.#save({});
+    return Object.keys(windows).map(Number);
+  }
+
+  /** @returns {Promise<Record<number, boolean>} */
+  static #load() {
+    return chrome.storage.session
+      .get('attachedWindows')
+      .then(({ attachedWindows }) => attachedWindows ?? {})
+      .catch(() => ({}));
+  }
+
+  /** @param {Promise<Record<number, boolean>} attachedWindows */
+  static #save(attachedWindows) {
+    return chrome.storage.session.set({ attachedWindows }).catch(console.warn);
+  }
+}
+
 const debugCalls = true;
 
-let settings = (() => {
+const settings = (() => {
   const map = { replaceTwitchChat: true };
 
   // load settings
@@ -82,15 +120,29 @@ function getPort() {
 // connect to port
 function connectPort() {
   port = chrome.runtime.connectNative(appName);
-  console.log('port connected');
-
-  detachedWindowsCache = {};
+  console.debug('port connected');
 
   port.onMessage.addListener(msg => {
-    console.log(msg);
+    if (typeof msg === 'object' && msg.type === 'status') {
+      switch (msg.status) {
+        case 'exiting-host':
+          console.info(
+            `Native host is exiting: '${msg.reason ?? '<unknown>'}'`,
+          );
+          break;
+        default:
+          console.log(msg);
+          break;
+      }
+    } else {
+      console.log(msg);
+    }
   });
-  port.onDisconnect.addListener(xd => {
-    console.log('port disconnected', (xd | {}).error, chrome.runtime.lastError);
+  port.onDisconnect.addListener(e => {
+    console.debug(
+      'port disconnected',
+      e?.error ?? e ?? chrome.runtime.lastError,
+    );
 
     port = null;
   });
@@ -107,73 +159,71 @@ function disconnectPort() {
 }
 
 // tab activated
-chrome.tabs.onActivated.addListener(activeInfo => {
-  chrome.tabs.get(activeInfo.tabId, tab => {
-    if (!tab || !tab.url) return;
+chrome.tabs.onActivated.addListener(async activeInfo => {
+  const tab = await chrome.tabs.get(activeInfo.tabId);
+  if (!tab || !tab.url) return;
 
-    chrome.windows.get(tab.windowId, {}, window => {
-      if (!window.focused) return;
+  const window = await chrome.windows.get(tab.windowId, {});
+  if (!window.focused) return;
 
-      if (debugCalls) console.log('onActivated');
+  if (debugCalls) console.log('onActivated');
 
-      onTabSelected(tab.url, tab);
-    });
-  });
+  await onTabSelected(tab.url, tab);
 });
 
 // url changed
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (!tab.highlighted) return;
 
-  chrome.windows.get(tab.windowId, {}, window => {
-    if (!window.focused) return;
+  const window = await chrome.windows.get(tab.windowId, {});
+  if (!window.focused) return;
 
-    if (debugCalls) console.log('onUpdated');
+  if (debugCalls) console.log('onUpdated');
 
-    onTabSelected(tab.url, tab);
-  });
+  onTabSelected(tab.url, tab);
 });
 
 // tab detached
-chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
+chrome.tabs.onDetached.addListener(async (tabId, detachInfo) => {
   if (debugCalls) console.log('onDetached');
 
-  tryDetach(detachInfo.oldWindowId);
+  await tryDetach(detachInfo.oldWindowId);
 });
 
 // tab closed
-chrome.windows.onRemoved.addListener(windowId => {
+chrome.windows.onRemoved.addListener(async windowId => {
   if (debugCalls) console.log('onRemoved');
 
-  tryDetach(windowId);
+  await tryDetach(windowId);
 });
 
 // window selected
-chrome.windows.onFocusChanged.addListener(windowId => {
+chrome.windows.onFocusChanged.addListener(async windowId => {
   console.log(windowId);
   if (windowId == -1) return;
 
   // this returns all tabs when the query fails
-  chrome.tabs.query({ windowId: windowId, highlighted: true }, tabs => {
-    if (tabs.length === 1) {
-      let tab = tabs[0];
-
-      chrome.windows.get(tab.windowId, window => {
-        if (debugCalls) console.log('onFocusChanged');
-
-        onTabSelected(tab.url, tab);
-      });
-    }
+  const tabs = await chrome.tabs.query({
+    windowId: windowId,
+    highlighted: true,
   });
+  if (tabs.length === 1) {
+    let tab = tabs[0];
+
+    const window = await chrome.windows.get(tab.windowId);
+    if (debugCalls) console.log('onFocusChanged');
+
+    await onTabSelected(tab.url, tab);
+  }
 });
 
 // attach or detach from tab
-function onTabSelected(url, tab) {
+async function onTabSelected(url, tab) {
   let channelName = matchChannelName(url);
 
   if (!channelName) {
     // detach from window
-    tryDetach(tab.windowId);
+    await tryDetach(tab.windowId);
   }
 }
 
@@ -186,18 +236,18 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
       callback(settings.all());
       break;
     case 'set-setting':
-      settings.set(message.key, message.value);
+      (async () => {
+        settings.set(message.key, message.value);
 
-      for (let id in attachedWindows) {
-        tryDetach(id);
-      }
-      updateBadge();
-
+        for (const id of await AttachedWindows.detachAll()) {
+          // they're already cleared
+          await sendDetach(id);
+        }
+        updateBadge();
+      })();
       break;
     case 'get-os':
-      chrome.runtime.getPlatformInfo(info => {
-        callback(info.os);
-      });
+      chrome.runtime.getPlatformInfo(info => callback(info.os));
 
       // We need to return true here so that `callback` will remain valid
       // after this function returns. This behavior is documented here:
@@ -227,23 +277,22 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
       if (!sender.tab.highlighted) return;
 
       // is window focused
-      chrome.windows.get(sender.tab.windowId, {}, window => {
+      chrome.windows.get(sender.tab.windowId, {}, async window => {
         if (!window.focused) return;
 
         // get zoom value
-        chrome.tabs.getZoom(sender.tab.id, zoom => {
-          let size = {
-            x: message.rect.x * zoom,
-            pixelRatio: 1,
-            width: Math.floor(message.rect.width * zoom),
-            height: Math.floor(message.rect.height * zoom),
-          };
+        const zoom = await chrome.tabs.getZoom(sender.tab.id);
+        let size = {
+          x: message.rect.x * zoom,
+          pixelRatio: 1,
+          width: Math.floor(message.rect.width * zoom),
+          height: Math.floor(message.rect.height * zoom),
+        };
 
-          // attach to window
-          tryAttach(sender.tab.windowId, window.state == 'fullscreen', {
-            name: matchChannelName(sender.tab.url),
-            size: size,
-          });
+        // attach to window
+        await tryAttach(sender.tab.windowId, window.state == 'fullscreen', {
+          name: matchChannelName(sender.tab.url),
+          size: size,
         });
       });
       break;
@@ -254,7 +303,7 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
 });
 
 // attach chatterino to a chrome window
-function tryAttach(windowId, fullscreen, data) {
+async function tryAttach(windowId, fullscreen, data) {
   console.log('tryAttach ' + windowId);
 
   data.action = 'select';
@@ -275,42 +324,47 @@ function tryAttach(windowId, fullscreen, data) {
     port.postMessage(data);
   }
 
-  attachedWindows[windowId] = {};
+  await AttachedWindows.markAttached(windowId);
 }
 
-// detach chatterino from a chrome window
-function tryDetach(windowId) {
-  if (
-    attachedWindows[windowId] === undefined &&
-    detachedWindowsCache[windowId] !== undefined
-  ) {
-    return;
+/**
+ * Detach chatterino from a chrome window
+ * @param {number} windowId
+ */
+async function tryDetach(windowId) {
+  if (await AttachedWindows.isAttached(windowId)) {
+    sendDetach(windowId);
+    await AttachedWindows.markDetatched(windowId);
   }
+}
 
-  detachedWindowsCache[windowId] = {};
+function sendDetach(winID) {
+  console.log('sendDetach', { winID });
 
-  console.log('tryDetach ' + windowId);
-
-  let port = getPort();
-
+  const port = getPort();
   if (port) {
-    port.postMessage({ action: 'detach', version: 0, winId: '' + windowId });
-  }
-
-  if (attachedWindows[windowId] !== undefined) {
-    delete attachedWindows[windowId];
+    port.postMessage({ action: 'detach', version: 0, winId: winID.toString() });
   }
 }
 
 function updateBadge() {
-  chrome.browserAction.setBadgeText({
+  chrome.action.setBadgeText({
     text: settings.all().replaceTwitchChat ? '' : 'off',
   });
 }
 
-// The previous state sent to chatterino
-let previousTabs = new Set();
-function syncTabs() {
+function getPreviousTabs() {
+  return chrome.storage.session
+    .get('previousTabs')
+    .then(({ previousTabs }) => new Set(previousTabs ?? []))
+    .catch(() => new Set());
+}
+
+async function setPreviousTabs(tabs) {
+  await chrome.storage.session.set({ previousTabs: [...tabs] });
+}
+
+async function syncTabs() {
   function compareTabs(lhs, rhs) {
     if (lhs.size !== rhs.size) {
       return false;
@@ -324,21 +378,24 @@ function syncTabs() {
     return true;
   }
 
-  chrome.tabs.query({ url: '*://*.twitch.tv/*' }, tabs => {
-    const currentTabs = new Set(
-      tabs.map(t => matchChannelName(t.url)).filter(Boolean)
-    );
-    if (compareTabs(previousTabs, currentTabs)) {
-      return;
-    }
-    previousTabs = currentTabs;
-    console.log('sending updated tabs:', currentTabs);
+  let previousTabs = await getPreviousTabs();
 
-    const port = getPort();
-    if (port) {
-      port.postMessage({ action: 'sync', twitchChannels: [...currentTabs] });
-    }
-  });
+  const tabs = await chrome.tabs.query({ url: '*://*.twitch.tv/*' });
+  const currentTabs = new Set(
+    tabs.map(t => matchChannelName(t.url)).filter(Boolean),
+  );
+  if (compareTabs(previousTabs, currentTabs)) {
+    return;
+  }
+  previousTabs = currentTabs;
+  console.log('sending updated tabs:', currentTabs);
+
+  const port = getPort();
+  if (port) {
+    port.postMessage({ action: 'sync', twitchChannels: [...currentTabs] });
+  }
+
+  await setPreviousTabs(previousTabs);
 }
 syncTabs();
 
